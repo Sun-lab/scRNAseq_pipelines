75e0f1d704bb7375b1ef0c297cbd8644f268a692
Wei Sun <weisun2013@gmail.com>
2018-10-05 01:17:01 -0700

    update pipeline
    Remove genes that are expressed (with 2 or more UMI) in less than 10 cells. Perform PCA and subequent tSNE and clustering using 920 genes.

--- dronc/SOURCE.R
+++ dronc/SOURCE.R
@@ -814,8 +814,53 @@ get_uniq = function(mm){
 
 
 
-
-
-
 ####
+
+
+gg.heatmap <- function(t2, ylab){
+  #create a new variable from incidence
+  if(max(t2$value) > 1000){
+    t2$valueFactor = cut(t2$value, 
+                         breaks = c(-1,0,5,10,50,100,1000,max(t2$value)),
+                         labels=c("0","1-5","6-10","11-50","51-100","101-1000",">1000"))
+  }else{
+    t2$valueFactor = cut(t2$value, 
+                         breaks = c(-1,0,5,10,50,100,max(t2$value)),
+                         labels=c("0","1-5","6-10","11-50","51-100","101-1000"))
+  }
+  
+  #change level order
+  t2$valueFactor = factor(as.character(t2$valueFactor),
+                          levels=rev(levels(t2$valueFactor)))
+  
+  y.axis.size = 8
+  if(nrow(t2) > 700)   y.axis.size = 7
+  if(nrow(t2) > 800)   y.axis.size = 6
+  
+  col1 = c("#f46d43","#fdae61","#fee08b","#e6f598","#abdda4","#ddf1da")
+  col2 = c("#d53e4f", col1)
+  
+  if(max(t2$value) > 1000){
+    col2use = col2
+  }else{
+    col2use = col1
+  }
+  
+  g1 = ggplot(t2, aes(Cell_Type, cluster)) + 
+    theme_bw() +
+    geom_tile(aes(fill = valueFactor), color = "white", size=0.25) +
+    scale_fill_manual(values=col2use) +  
+    ylab(ylab) + xlab("cell types") +
+    theme(legend.title=element_blank(),
+          legend.text=element_text(colour="grey40",size=9,face="bold"),
+          legend.key.height=grid::unit(0.8,"cm"),
+          legend.key.width=grid::unit(0.4,"cm"),
+          plot.title = element_text(size=16),
+          axis.title = element_text(size=14, face="bold"),
+          axis.text.x = element_text(angle = 90, hjust = 1), 
+          axis.text.y = element_text(size=y.axis.size), 
+          panel.border=element_blank())
+  g1
+}
+
 
