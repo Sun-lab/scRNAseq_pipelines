2379529735784c48ff2778cb10d51b415ee1d2a2
pllittle <pllittle@email.unc.edu>
2019-02-07 14:25:21 -0500

    made kmeans reproducible

--- _brain_cell_type/step2_prepare_DE.R
+++ _brain_cell_type/step2_prepare_DE.R
@@ -1,7 +1,10 @@
+rm(list=ls())
 
-repo_dir  = "~/research/GitHub/scRNAseq_pipelines"
-work_dir  = file.path(repo_dir,"MTG")
-MTG_dir   = "~/research/scRNAseq/data/Allen_BI/human_MTG_gene_expression_matrices_2018-06-14"
+repo_dir	= "~/research/GitHub/scRNAseq_pipelines"
+work_dir	= file.path(repo_dir,"MTG")
+MTG_dir		= "~/research/scRNAseq/data/Allen_BI/human_MTG_gene_expression_matrices_2018-06-14"
+
+MTG_dir 	= "/pine/scr/p/l/pllittle/CS_eQTL/s3_Real/scRNAseq_pipelines/MTG"
 
 # ------------------------------------------------------------------
 # load data matrix 
@@ -9,8 +12,8 @@ MTG_dir   = "~/research/scRNAseq/data/Allen_BI/human_MTG_gene_expression_matrice
 
 setwd(MTG_dir)
 
-sce      = readRDS("final_sce.rds")
-clusters = readRDS("final_hvg_clust.rds")
+sce			= readRDS("final_sce.rds")
+clusters	= readRDS("final_hvg_clust.rds")
 
 dim(sce)
 dim(colData(sce))
@@ -32,7 +35,9 @@ clusters[1:2,1:5]
 table(clusters$sample_name == colData(sce)$sample_name)
 table(clusters$cell_type   == colData(sce)$cell_type)
 
-t1 = table(clusters$KM_15, clusters$cell_type)
+# opt_clust = paste0("KM_",15)
+opt_clust = paste0("KM_",18) # our clustering results differ slightly
+t1 = table(clusters[,opt_clust], clusters$cell_type)
 t1
 
 clusts = apply(t1, 2, function(v){union(which.max(v), which(v > 200))})
@@ -44,9 +49,9 @@ celltypes
 w2kp = NULL
 
 for(ct1 in celltypes){
-  ct.cond    = clusters$cell_type == ct1
-  clust.cond = clusters$KM_15 %in% clusts[[ct1]]
-  w2kp = c(w2kp, which(ct.cond & clust.cond))
+	ct.cond    = clusters$cell_type == ct1
+	clust.cond = clusters[,opt_clust] %in% clusts[[ct1]]
+	w2kp = c(w2kp, which(ct.cond & clust.cond))
 }
 length(w2kp)
 
