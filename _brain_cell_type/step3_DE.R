dcfb4ad03175c1d2d18f654a60803b0e0ae7c5c3
pllittle <pllittle@email.unc.edu>
2019-06-18 00:34:47 -0400

    corrected proportions

--- _brain_cell_type/step3_DE.R
+++ _brain_cell_type/step3_DE.R
@@ -93,7 +93,6 @@ GTF_calc_gene_length = function(work_dir,rsem_gtf_fn){
 	# Output
 	gtf
 }
-
 MAST_DEgenes = function(work_dir,num_genes=NULL,sce_obj,one_cell_type){
   
 	if(FALSE){
@@ -174,18 +173,64 @@ MAST_DEgenes = function(work_dir,num_genes=NULL,sce_obj,one_cell_type){
 	return(NULL)
 }
 
+# ICeDT related Functions
+q90 = function(vv){
+	qs = quantile(vv, probs=c(0.10, 0.90))
+	qs[2] - qs[1]
+}
+plot_log1p = function(x, y, ...) {
+	smoothScatter(log(x+1e-5), log(y+1e-5), xlim=c(-5, 10), ylim=c(-5, 10), ...)
+	legend("bottom",bty="n",legend=sprintf("Pearson correlation = %.2f",
+		cor(log(x+1e-5), log(y+1e-5))))
+}
+ICeDT_consistency = function(sig,bulk,ICeDT_out){
+
+	p1 = ICeDT_out$cProb
+	prop_icedt = t(ICeDT_out$rho)[,-1]
+	# dim(p1)
+	p1 = data.matrix(p1)
+	
+	par(mfrow=c(2,3),mar=c(4,4,1,0.5),bty="n")
+	# par(mfrow=c(1,3))
+	plot(density(c(p1))$y,main="",xlim=c(0,1),xlab="probability consistent",
+		ylab="density",type="n")
+	lines(density(c(p1)), lty=1, col="black")
+	legend("topright", c("no weight"), lty=c(1,2),col=c("black"), bty="n")
+	plot(apply(p1,1,median),apply(p1,1,q90),xlab="median prob. consistent",
+		ylab="90 percentile - 10 percentile",main="Gene Consistency across subjects",
+		xlim=c(0,1),ylim=c(0,1))
+	boxplot(prop_icedt,main = "Simulation - ICeDT")
+	# par(mfrow=c(1,1))
+  
+	predicted_bulk_w0 = sig %*% t(prop_icedt)
+	p1_cutoffs = quantile(p1,c(1/3,2/3)); p1_cutoffs
+  
+	# par(mfrow=c(1,3))
+	plot_log1p(x = c(predicted_bulk_w0)[p1 < p1_cutoffs[1]],
+		y = c(bulk)[p1 < p1_cutoffs[1]], 
+		xlab = "Predicted gene expression",ylab = "Observed gene expression",
+		sub = "model w/ weight",main = "low prob of being consistent")
+	plot_log1p(x = c(predicted_bulk_w0)[p1 >= p1_cutoffs[1] & p1 <= p1_cutoffs[2]],
+		y = c(bulk)[p1 >= p1_cutoffs[1] & p1 <= p1_cutoffs[2]], 
+		xlab = "Predicted gene expression",ylab = "Observed gene expression",
+		sub = "model w/ weight",main = "med prob of being consistent")
+	plot_log1p(x = c(predicted_bulk_w0)[p1 > p1_cutoffs[2]],
+		y = c(bulk)[p1 > p1_cutoffs[2]],
+		xlab = "Predicted gene expression",ylab = "Observed gene expression",
+		sub = "model w/ weight",main = "high prob of being consistent")
+	par(mfrow=c(1,1),mar=c(5,4,4,2)+0.1,bty="o")
+}
+
+
 
 # ------------------------------------------------------------
 # Import and Prep Data
 # ------------------------------------------------------------
 # Import gtf with gene lengths
 rsem_fn = "rsem_GRCh38.p2.gtf"
-gtf_fn = "rsem_GRCh38.p2.rds"
-if( !file.exists(gtf_fn) ){
-	gtf = GTF_calc_gene_length(work_dir = MTG_dir,rsem_gtf_fn = rsem_fn)
-	saveRDS(gtf,gtf_fn)
-}
-gtf = readRDS(gtf_fn)
+# gtf_fn = "rsem_GRCh38.p2.rds"
+gtf = GTF_calc_gene_length(work_dir = MTG_dir,rsem_gtf_fn = rsem_fn)
+head(gtf)
 
 sce = readRDS(file.path(rawData_dir,"final_sce_filtered_by_kmeans.rds"))
 # sce = readRDS(file.path(MTG_dir,"final_sce_filtered_by_kmeans.rds"))
@@ -269,8 +314,8 @@ saveRDS(gtf,"DE_gene_anno.rds")
 # ------------------------------------------------------------
 
 dat = smart_df(rowData(sce)[,c("gene","chromosome","entrez_id")])
-fdr_thres = 1e-3
-logFC_thres = log(2)
+fdr_thres 		= 1e-3
+logFC_thres	 	= log(2)
 
 # Get each cell type's differentially expressed genes
 ct_genes = list()
@@ -300,12 +345,13 @@ for(ct in cell_types){
 sapply(cts_genes,length)
 saveRDS(cts_genes,"cts_genes.rds")
 
-# Get approximate top 100 marker genes per cell type
+# Get approximate top 120 marker genes per cell type
 ## Rather than arbitrarily selecting 100 genes, I apply 
 ##	a quantile threshold on both logFC and FDR_qvalue to 
 ##	select approximately 100 genes per cell type.
 set.seed(1)
 mark_genes = list()
+num_mgene_per_ct = 120 # number of marker genes selected per cell type
 for(ct in cell_types){
 	# ct = cell_types[1]
 	cat(paste0(ct,": "))
@@ -321,11 +367,11 @@ for(ct in cell_types){
 		qu = runif(1,low_bound,1)
 		num_rows = nrow(rds[which(rds$logFC > quantile(rds$logFC,qu) 
 			& rds$FDR_qvalue < quantile(rds$FDR_qvalue,1-qu)),])
-		if( num_rows > 100 ){
+		if( num_rows > num_mgene_per_ct ){
 			# Tighten bounds
 			low_bound = qu # mean(c(low_bound[1],qu))
 			cat(paste0(round(low_bound,3)," "))
-			if( num_rows <= 110 ) break
+			if( num_rows <= num_mgene_per_ct+10 ) break
 		}
 	}
 	cat("\n")
@@ -347,57 +393,67 @@ dev.off()
 # Calculate TPM and get signature matrix: Based on Chong Jin's code
 # ------------------------------------------------------------
 table(colData(sce)$cell_type)
-sig_cts = matrix(nrow = nrow(sce),ncol = length(cell_types))
-colnames(sig_cts) = cell_types
-rownames(sig_cts) = rownames(sce)
+
+# Construct signature matrix
+SIG = matrix(nrow = nrow(sce),ncol = length(cell_types))
+colnames(SIG) = cell_types
+rownames(SIG) = rownames(sce)
 for(ct in cell_types){
 	print(ct)
-	sig_cts[,ct] = rowSums(counts(sce)[,colData(sce)$cell_type == ct])
+	SIG[,ct] = rowSums(counts(sce)[,colData(sce)$cell_type == ct])
 }
-dim(sig_cts)
-sig_cts = sig_cts / rowData(sce)$gene_length
-sig_cts = 1e6 * apply(sig_cts,2,function(xx) xx / sum(xx)) 
-sig_cts = sig_cts[sort(as.character(unlist(mark_genes))),]
+dim(SIG)
+
+# Subset marker genes
+SIG = SIG[sort(as.character(unlist(mark_genes))),]
+dim(SIG)
 
 # Get ENSG id
-dat	= smart_df(rowData(sce)[,c("gene","chromosome","entrez_id")])
-dat = dat[which(dat$gene %in% rownames(sig_cts)),]
+dat	= gtf[,c("gene_symbol","chromosome","entrez_id")]
 rownames(dat) = NULL
-dat = dat[match(rownames(sig_cts),dat$gene),]
-
+dat = dat[match(rownames(SIG),dat$gene_symbol),]
 biomaRt::listEnsembl()
 ensembl = biomaRt::useMart("ensembl")
 dd = biomaRt::listDatasets(ensembl); dim(dd); dd[1:5,]
 dd[grep("hsap",dd$dataset),]
 ee = biomaRt::useDataset("hsapiens_gene_ensembl",mart = ensembl)
-ff = biomaRt::listFilters(ee); dim(ff); ff[1:5,]
-aa = biomaRt::listAttributes(ee); dim(aa); aa[1:5,]
+# ff = biomaRt::listFilters(ee); dim(ff); ff[1:5,]
+# aa = biomaRt::listAttributes(ee); dim(aa); aa[1:5,]
 
 attr_string = c("hgnc_symbol","ensembl_gene_id","external_gene_name",
 				"entrezgene","chromosome_name","start_position",
 				"end_position","strand","gene_biotype")
 filters = c("hgnc_symbol","chromosome_name","entrezgene")
-values = list(dat$gene,dat$chromosome,dat$entrez_id)
+values = list(dat$gene_symbol,dat$chromosome,dat$entrez_id)
 gene_anno = biomaRt::getBM(attributes = attr_string,
 						filters = filters, 
 						values = values,
 						mart = ee)
 gene_anno = unique(gene_anno)
 dim(gene_anno); gene_anno[1:5,]
-smart_table(rownames(sig_cts) %in% gene_anno$hgnc_symbol)
+smart_table(rownames(SIG) %in% gene_anno$hgnc_symbol)
 
 # What are the missing genes?
-miss_genes = setdiff(rownames(sig_cts),gene_anno$hgnc_symbol)
+miss_genes = setdiff(rownames(SIG),gene_anno$hgnc_symbol)
 miss_genes
 
+# Subset and order genes
+inter_genes = intersect(rownames(SIG),gene_anno$hgnc_symbol)
+gene_anno = gene_anno[which(gene_anno$hgnc_symbol %in% inter_genes),]
+gtf = gtf[which(gtf$gene_symbol %in% inter_genes),]
+SIG = SIG[which(rownames(SIG) %in% inter_genes),]
+gtf2 = gtf[,c("gene_symbol","gene_length")]
+	gtf2 = name_change(gtf2,"gene_symbol","hgnc_symbol")
+gene_anno = smart_merge(gene_anno,gtf2[,c("hgnc_symbol","gene_length")])
+gene_anno = gene_anno[match(rownames(SIG),gene_anno$hgnc_symbol),]
+
 # Output
-saveRDS(list(anno = gene_anno,sig_cts = sig_cts),"signature.rds")
+saveRDS(list(anno = gene_anno,SIG = SIG),"signature_MTG.rds")
 
 
 # ------------------------------------------------------------
-# Deconvolution: Based on Chong Jin's deconvolution.Rmd code
+# Import CMC Bulk RNA, get gene lengths
 # ------------------------------------------------------------
-# Import CMC Bulk RNA, get gene lengths, calculate TPM
 bulk = readRDS("CMC_MSSM-Penn-Pitt_Paul_geneExpressionRaw.rds")$so1
 dim(bulk); bulk[1:5,1:5]
 
@@ -407,131 +463,116 @@ exons_list_per_gene = GenomicFeatures::exonsBy(exdb,by = "gene")
 tmp_df = smart_df(ensembl_gene_id = names(exons_list_per_gene),
 				gene_length = as.numeric(sum(width(GenomicRanges::reduce(exons_list_per_gene)))))
 tmp_df[1:5,]
-
 all(tmp_df$ensembl_gene_id %in% rownames(bulk))
 all(tmp_df$ensembl_gene_id == rownames(bulk))
-bulk = bulk / tmp_df$gene_length
-bulk = 1e6 * apply(bulk,2,function(xx) xx/sum(xx))
-bulk[1:5,1:5] # tpm unit
 
-# Import signature matrix and annotation
-rds = readRDS("signature.rds")
-gene_anno = rds$anno
-sig_cts = rds$sig_cts
-rm(rds)
+# Subset/Order marker genes
+inter_genes2 	= intersect(tmp_df$ensembl_gene_id,gene_anno$ensembl_gene_id)
+gene_anno2 		= gene_anno[which(gene_anno$ensembl_gene_id %in% inter_genes2),]
+bulk 					= bulk[which(rownames(bulk) %in% inter_genes2),]
+bulk 					= bulk[match(gene_anno2$ensembl_gene_id,rownames(bulk)),]
+tmp_df 				= tmp_df[which(tmp_df$ensembl_gene_id %in% inter_genes2),]
+tmp_df 				= tmp_df[match(gene_anno2$ensembl_gene_id,tmp_df$ensembl_gene_id),]
 
-# Get gene intersection, subset bulk and sig_cts, matching gene order
-smart_table(rownames(bulk) %in% gene_anno$ensembl_gene_id)
-inter_genes = intersect(rownames(bulk),gene_anno$ensembl_gene_id)
-gene_anno = gene_anno[which(gene_anno$ensembl_gene_id %in% inter_genes),]
+saveRDS(list(anno = tmp_df,bulk = bulk),"bulk_marker_MTG.rds")
 
-bulk = bulk[which(rownames(bulk) %in% inter_genes),]
-bulk = bulk[match(gene_anno$ensembl_gene_id,rownames(bulk)),]
 
-sig_cts = sig_cts[which(rownames(sig_cts) %in% gene_anno$hgnc_symbol),]
-sig_cts = sig_cts[match(gene_anno$hgnc_symbol,rownames(sig_cts)),]
+# ------------------------------------------------------------
+# Deconvolution: Based on Chong Jin's deconvolution.Rmd code
+# ------------------------------------------------------------
+# Refer to EPIC paper for variable definitions (bb = bulk,cc = signature,pp = cell type proportions)
+bulk_rds 	= readRDS("bulk_marker_MTG.rds")
+sig_rds 	= readRDS("signature_MTG.rds")
+inter_genes3 	= intersect(bulk_rds$anno$ensembl_gene_id,sig_rds$anno$ensembl_gene_id)
+bulk_rds$anno = bulk_rds$anno[which(bulk_rds$anno$ensembl_gene_id %in% inter_genes3),]
+bulk_rds$bulk = bulk_rds$bulk[which(rownames(bulk_rds$bulk) %in% inter_genes3),]
+sig_rds$anno 	= sig_rds$anno[which(sig_rds$anno$ensembl_gene_id %in% inter_genes3),]
+sig_rds$SIG 	= sig_rds$SIG[which(rownames(sig_rds$SIG) %in% sig_rds$anno$hgnc_symbol),]
+rownames(sig_rds$SIG) = sig_rds$anno$ensembl_gene_id
 
-all(rownames(sig_cts) == gene_anno$hgnc_symbol)
-all(rownames(bulk) == gene_anno$ensembl_gene_id)
+bb_tpm = apply(bulk_rds$bulk,2,function(xx) xx / bulk_rds$anno$gene_length)
+bb_tpm = 1e6 * apply(bb_tpm,2,function(xx) xx / sum(xx))
+cc_tpm = apply(sig_rds$SIG,2,function(xx) xx / sig_rds$anno$gene_length)
+cc_tpm = 1e6 * apply(cc_tpm,2,function(xx) xx / sum(xx))
 
-# Rename rownames of sig_cts to match bulk
-rownames(sig_cts) = gene_anno$ensembl_gene_id
+cell_sizes = colSums(apply(sig_rds$SIG,2,function(xx) xx / sig_rds$anno$gene_length))
 
 # Run ICeDT
 pack = "ICeDT"
 if( !(pack %in% installed.packages()[,"Package"]) ){
 	devtools::install_github("Sun-lab/ICeDT")
 }
-icedt_fn = "icedt.rds"
 date()
-fitw0 = ICeDT::ICeDT(Y = bulk,Z = sig_cts,tumorPurity = rep(0,ncol(bulk)),
+fit = ICeDT::ICeDT(Y = bb_tpm,Z = cc_tpm,tumorPurity = rep(0,ncol(bb_tpm)),
 	refVar = NULL,rhoInit = NULL,maxIter_prop = 4e3,maxIter_PP = 4e3,
 	rhoConverge = 1e-2)
 date()
-saveRDS(fitw0,icedt_fn)
-fitw0 = readRDS(icedt_fn)
-p1 = fitw0$cProb
-prop_icedt = t(fitw0$rho)[,-1]
-dim(p1)
-p1[1:2,1:5]
-p1 = data.matrix(p1)
-q90 <- function(v){
-	qs = quantile(v, probs=c(0.10, 0.90))
-	qs[2] - qs[1]
-}
-
-pdf("probConsistent_GeneSet.pdf",width=13,height=4)
-
-par(mar=c(5,4,1,1),bty="n",mfrow=c(1,3),cex=0.8)
-plot(density(c(p1))$y,main="",xlim=c(0,1),xlab="probability consistent",
-	ylab="density",type="n")
-lines(density(c(p1)), lty=1, col="black")
-legend("topright", c("no weight"), lty=c(1,2),col=c("black"), bty="n")
-plot(apply(p1,1,median),apply(p1,1,q90),xlab="median prob. consistent",
-	ylab="90 percentile - 10 percentile",main="Gene Consistency across subjects")
-boxplot(prop_icedt,main = "MTG - ICeDT")
-par(mfrow=c(1,1))
-
-# Predicted vs Observed gene expression
-predicted_bulk_w0 = sig_cts %*% t(prop_icedt)
-p1_cutoffs = quantile(p1,c(1/3,2/3)); p1_cutoffs
-plot_log1p = function(x, y, ...) {
-	smoothScatter(log(x+1e-5), log(y+1e-5), xlim=c(-5, 10), ylim=c(-5, 10), ...)
-	legend("bottomright",bty="n",legend=sprintf("Pearson correlation = %.2f",
-		cor(log(x+1e-5), log(y+1e-5))))
-}
-par(mar=c(5,4,1,1),bty="n",mfrow=c(1,3),cex=0.6)
-plot_log1p(x = c(predicted_bulk_w0)[p1 < p1_cutoffs[1]],
-		y = c(bulk)[p1 < p1_cutoffs[1]], 
-		xlab = "Predicted gene expression",ylab = "Observed gene expression",
-		sub = "model w/ weight",main = "low prob of being consistent")
-plot_log1p(x = c(predicted_bulk_w0)[p1 >= p1_cutoffs[1] & p1 <= p1_cutoffs[2]],
-		y = c(bulk)[p1 >= p1_cutoffs[1] & p1 <= p1_cutoffs[2]], 
-		xlab = "Predicted gene expression",ylab = "Observed gene expression",
-		sub = "model w/ weight",main = "med prob of being consistent")
-plot_log1p(x = c(predicted_bulk_w0)[p1 > p1_cutoffs[2]],
-		y = c(bulk)[p1 > p1_cutoffs[2]],
-		xlab = "Predicted gene expression",ylab = "Observed gene expression",
-		sub = "model w/ weight",main = "high prob of being consistent")
-
-dev.off()
+pp_bar_icedt = t(fit$rho)[,-1]
+# calculate p_hat using cell_sizes
+pp_hat_icedt = t(apply(pp_bar_icedt,1,function(xx){yy = xx / cell_sizes; yy / sum(yy)}))
+# Look at distribution of pp_bar and pp_hat
+summary(pp_bar_icedt)
+summary(pp_hat_icedt)
 
 # Run CIBERSORT
-write.table(cbind(rowname=rownames(sig_cts),sig_cts),
-			file = file.path(MTG_dir,"signature_MTG.txt"),
-			sep = "\t",quote = FALSE,row.names = FALSE)
-write.table(cbind(rowname=rownames(bulk),bulk),
-			file = file.path(MTG_dir,"mixture_CMC.txt"),
-			sep = "\t",quote = FALSE,row.names = FALSE)
+sig_fn = file.path(MTG_dir,"signature_MTG.txt")
+mix_fn = file.path(MTG_dir,"mixture_CMC.txt")
+write.table(cbind(rowname=rownames(cc_tpm),cc_tpm),
+	file = sig_fn,sep = "\t",quote = FALSE,row.names = FALSE)
+write.table(cbind(rowname=rownames(bb_tpm),bb_tpm),
+	file = mix_fn,sep = "\t",quote = FALSE,row.names = FALSE)
 # Login to CIBERSORT website, uploaded above two files, 
 #	specified no quantile normalization, ran 1000 permutations
-cib = smart_RT(file.path(MTG_dir,"CIBERSORT.Output_Job4.txt"),
-			sep = "\t",header = TRUE)
-dim(cib); cib[1:5,]
-prop_cib = as.matrix(cib[,2:7])
-
-# Compare proportions
-pdf("compare_props.pdf",width=9,height=5)
-
-# Proportions across cell types
-par(mfrow=c(1,2),mar=c(5,4,1,1))
-boxplot(prop_icedt,main = "MTG - ICeDT")
-boxplot(prop_cib,main = "MTG - CIBERSORT")
-
-# Proportions by cell type
-cell_types = colnames(prop_cib)
-par(mar=c(5,4,1,1),bty="n",mfrow=c(2,3),cex=0.8)
-for(ct in cell_types){
-	tmp_range = range(c(prop_icedt[,ct],prop_cib[,ct]))
-	plot(prop_icedt[,ct],prop_cib[,ct],xlab="prop est in ICeDT",
-		ylab="prop est in CIBERSORT",pch=16,cex=1.1,cex.lab=1.1,
-		col=rgb(0,0,0,0.25),xlim=tmp_range,ylim=tmp_range,main=ct)
-	abline(a=0,b=1,lty=2,col="blue",lwd=2)
-}
-
-saveRDS(list(ICeDT = prop_icedt,CIBERSORT = prop_cib),"prop.rds")
-
+# OR Request CIBERSORT.R file from website, install dependent packages
+cibersort_src_fn = "~/github/CSeQTL/R/CIBERSORT.R"
+# cibersort_src_fn = "." # whichever directory contains CIBERSORT.R
+source(cibersort_src_fn)
+print(date())
+results = CIBERSORT(sig_matrix = sig_fn,mixture_file = mix_fn,
+	perm = 0,QN = FALSE,absolute = FALSE,abs_method = 'sig.score')
+print(date())
+unlink(sig_fn)
+unlink(mix_fn)
+QQ = ncol(cc_tpm)
+pp_bar_ciber = results[,seq(QQ)]
+# calculate p_hat using cell sizes
+pp_hat_ciber = t(apply(pp_bar_ciber,1,function(xx){yy = xx / cell_sizes; yy / sum(yy)}))
+	
+# Output MTG deconvolution results
+pdf("MTG_bulk_deconvolution_summary.pdf",height=8,width=12)
+	
+	ICeDT_consistency(sig = cc_tpm,bulk = bb_tpm,ICeDT_out = fit)
+	
+	par(mfrow=c(1,2),mar=c(4,4,1,0.5),oma=c(0,0,2,0),bty="n")
+	boxplot(pp_bar_icedt,main="Expression Proportions",ylim=c(0,1))
+	boxplot(pp_hat_icedt,main="Cell type Proportions",ylim=c(0,1))
+	mtext("ICeDT Results",outer=TRUE,cex=1.3)
+	par(mfrow=c(1,1),mar=c(5,4,4,2)+0.1,oma=rep(0,4),bty="o")
+	
+	par(mfrow=c(1,2),mar=c(4,4,1,0.5),oma=c(0,0,2,0),bty="n")
+	boxplot(pp_bar_ciber,main="Expression Proportions",ylim=c(0,1))
+	boxplot(pp_hat_ciber,main="Cell type Proportions",ylim=c(0,1))
+	mtext("CIBERSORT Results",outer=TRUE,cex=1.3)
+	par(mfrow=c(1,1),mar=c(5,4,4,2)+0.1,oma=rep(0,4),bty="o")
+	
+	par(mfrow=c(2,3),mar=c(4,4,1,0.5),bty="n")
+	for(ct in seq(QQ)){
+		tmp_range = range(c(pp_hat_ciber[,ct],pp_hat_icedt[,ct]))
+		plot(pp_hat_ciber[,ct],pp_hat_icedt[,ct],xlim=tmp_range,ylim=tmp_range,
+			xlab="CIBERSORT",ylab="ICeDT",main=colnames(pp_hat_ciber)[ct])
+		abline(a=0,b=1,lty=2,lwd=2,col="red")
+	}
+	par(mfrow=c(1,1),mar=c(5,4,4,2)+0.1,bty="o")
+	
 dev.off()
+
+saveRDS(list(ICeDT = pp_hat_icedt,CIBERSORT = pp_hat_ciber),"prop_MTG.rds")
+
+
+
+
+
+
 
 
 
@@ -550,6 +591,8 @@ dev.off()
 # ------------------------------------------------------------
 # Re-run deconvolution with intersection of MTG and psychENCODE signature genes
 # ------------------------------------------------------------
+if(FALSE){
+
 fdr_thres = 1e-3
 logFC_thres = log(2)
 
@@ -820,27 +863,7 @@ saveRDS(list(ICeDT = prop_icedt,CIBERSORT = prop_cib),"prop_int.rds")
 
 dev.off()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 sessionInfo()
 
