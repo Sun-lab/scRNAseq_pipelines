d12a087fa9b40765badd13143bc11f86b35d763f
mqzhanglab <46196207+mqzhanglab@users.noreply.github.com>
2019-09-12 08:22:16 -0700

    minor fix

--- DE_Autism/0_data_preprocessing.R
+++ DE_Autism/0_data_preprocessing.R
@@ -2,9 +2,7 @@
 
 setwd("/fh/fast/sun_w/mengqi/1.Testing_scRNAseq/")
 
-
 exprM=read.table("../Data_PRJNA434002/exprMatrix.tsv.gz",header=TRUE,row.names=1)
-exprM=as.matrix(exprM)
 saveRDS(exprM,"../Data_PRJNA434002/exprMatrix.rds")
 
 exprM100=as.matrix(exprM[,seq(1,1045)*100])
