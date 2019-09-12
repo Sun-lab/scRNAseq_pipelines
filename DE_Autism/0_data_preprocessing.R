5aee55559a257a0fb23bd913ccaff370fe9d771b
mqzhanglab <46196207+mqzhanglab@users.noreply.github.com>
2019-09-12 08:39:19 -0700

    generate another testing files M3k10

--- DE_Autism/0_data_preprocessing.R
+++ DE_Autism/0_data_preprocessing.R
@@ -2,12 +2,22 @@
 
 setwd("/fh/fast/sun_w/mengqi/1.Testing_scRNAseq/")
 
+#change the original file into rds for better read-writing-storage
 exprM=read.table("../Data_PRJNA434002/exprMatrix.tsv.gz",header=TRUE,row.names=1)
+
 saveRDS(exprM,"../Data_PRJNA434002/exprMatrix.rds")
 
+
+#generate a 1% percentage cell subfiles, for testing
 exprM100=as.matrix(exprM[,seq(1,1045)*100])
 saveRDS(exprM100,"../Data_PRJNA434002/exprMatrix100.rds")
 
 meta=read.table("../Data_PRJNA434002/meta.tsv",header = TRUE, sep = "\t")
 meta100=meta[seq(1,1045)*100,]
 saveRDS(meta100,"../Data_PRJNA434002/meta100.tsv")
+
+#generate another 10% percentage cells with first 3000 genes, for testing
+meta10=meta[seq(1,10455)*10,]
+saveRDS(meta100,"../Data_PRJNA434002/meta10.tsv")
+exprM3k10=exprM[1:3000,seq(1,10455)*10]
+saveRDS(exprM3k10,"../Data_PRJNA434002/exprMatrix3k10.rds")
