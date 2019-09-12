18828601a5c20eaa0b7c914cdeadd0b972f31fb7
mqzhanglab <46196207+mqzhanglab@users.noreply.github.com>
2019-09-12 09:15:14 -0700

    fix typo

--- DE_Autism/0_data_preprocessing.R
+++ DE_Autism/0_data_preprocessing.R
@@ -14,10 +14,10 @@ saveRDS(exprM100,"../Data_PRJNA434002/exprMatrix100.rds")
 
 meta=read.table("../Data_PRJNA434002/meta.tsv",header = TRUE, sep = "\t")
 meta100=meta[seq(1,1045)*100,]
-saveRDS(meta100,"../Data_PRJNA434002/meta100.tsv")
+saveRDS(meta100,"../Data_PRJNA434002/meta100.rds")
 
 #generate another 10% percentage cells with first 3000 genes, for testing
 meta10=meta[seq(1,10455)*10,]
-saveRDS(meta100,"../Data_PRJNA434002/meta10.tsv")
+saveRDS(meta100,"../Data_PRJNA434002/meta10.rds")
 exprM3k10=exprM[1:3000,seq(1,10455)*10]
 saveRDS(exprM3k10,"../Data_PRJNA434002/exprMatrix3k10.rds")
