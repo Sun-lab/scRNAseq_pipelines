47d41fdf667452e401957c21a1cd4a83e6d70743
mqzhanglab <meggy.q.zhang@gmail.com>
2019-09-18 22:01:27 -0700

    modified:   DE_Autism/1_autism-tSNE_plot.R 	deleted:    DE_Autism/tSNE_plots.pdf 	deleted:    DE_Autism/tSNE_plots_cor0.05xperp30.pdf 	deleted:    DE_Autism/tSNE_plots_cor0.2xperp50.pdf 	DE_Autism/0.1_data_preprocessing_checking percentage_of_0_in_gene.R 	DE_Autism/MAST_autism.Rmd 	DE_Autism/histogram_exprMatrix_stat.pdf 	DE_Autism/scree_plot.pdf DE_Autism/tsne_k=16/

--- DE_Autism/1_autism-tSNE_plot.R
+++ DE_Autism/1_autism-tSNE_plot.R
@@ -17,51 +17,49 @@ k = 50
 svd50=svds(exprM, k)
 #svd50=readRDS("../Data_PRJNA434002/svd50.rds")
 
+pdf("scree_plot.pdf",width = 6,height = 6)
+plot(svd50$d^2/sum(svd50$d^2), xlim = c(0, k),ylim=c(0,0.01), type = "b", pch = 16, xlab = "principal components", 
+     ylab = "variance explained", main="scree plot")
+dev.off()
+
+k=13
+svd50v=svd50$v[,1:k]
+
 xc=as.matrix(meta[,"Capbatch"])
 xs=as.matrix(meta[,"Seqbatch"])
 cor_c=matrix(ncol=1,nrow=k)
 cor_s=matrix(ncol=1,nrow=k)
+
 for(ik in 1:k){
-  y=svd50$v[,ik]
+  y=svd50v[,ik]
   ##calculate correlation of capbatch
   lmc = lm(y ~ as.factor(xc))
   cor_c[ik]=summary(lmc)$r.square
   ##calculate correlation of seqbatch
   lms = lm(y ~ as.factor(xs))
-  cor_s[ik]=summary(lms)$r.square
+  cor_s[ik]=sqrt(summary(lms)$r.square)
   
 }
 
-#cor 0.05 threshold perplexity 30
-flag=(abs(cor_c)<0.05 & abs(cor_s)<0.05) 
-svd50v=svd50$v[,flag==1]
-tsne=Rtsne(svd50v,dims=2, perplexity=30)
 
-pdf("tSNE_plots_cor0.05xperp30.pdf",height = 8,width = 8)
-plot(tsne$Y, cex=.1,main="tSNE-Capbatch",col=as.numeric(as.factor(meta[,"Capbatch"])))
-plot(tsne$Y, cex=.1,main="tSNE-Seqbatch",col=as.numeric(as.factor(meta[,"Seqbatch"])))
-plot(tsne$Y, cex=.2,main="tSNE-cluster",col=as.numeric(as.factor(meta[,"cluster"])))
-plot(tsne$Y, cex=.2,main="tSNE-individual",col=as.numeric(as.factor(meta[,"individual"])))
-dev.off()
-
-#cor 0.1 threshold perplexity 40
+#cor 0.1 threshold 
 flag=(abs(cor_c)<0.1 & abs(cor_s)<0.1) 
-svd50v=svd50$v[,flag==1]
-tsne=Rtsne(svd50v,dims=2, perplexity=40)
+svd50v0.1=svd50v[,flag==1]
+tsne=Rtsne(svd50v0.1,dims=2, perplexity=10)
 
-pdf("tSNE_plots_cor0.2xperp50.pdf",height = 8,width = 8)
+pdf("tSNE_plots_cor0.1_10.pdf",height = 8,width = 8)
 plot(tsne$Y, cex=.1,main="tSNE-Capbatch",col=as.numeric(as.factor(meta[,"Capbatch"])))
 plot(tsne$Y, cex=.1,main="tSNE-Seqbatch",col=as.numeric(as.factor(meta[,"Seqbatch"])))
 plot(tsne$Y, cex=.2,main="tSNE-cluster",col=as.numeric(as.factor(meta[,"cluster"])))
 plot(tsne$Y, cex=.2,main="tSNE-individual",col=as.numeric(as.factor(meta[,"individual"])))
 dev.off()
 
-#cor 0.2 threshold perplexity 50
+#cor 0.2 threshold 
 flag=(abs(cor_c)<0.2 & abs(cor_s)<0.2) 
-svd50v=svd50$v[,flag==1]
-tsne=Rtsne(svd50v,dims=2, perplexity=50)
+svd50v0.2=svd50v[,flag==1]
+tsne=Rtsne(svd50v0.2,dims=2, perplexity=10)
 
-pdf("tSNE_plots_cor0.2xperp50.pdf",height = 8,width = 8)
+pdf("tSNE_plots_cor0.2_10.pdf",height = 8,width = 8)
 plot(tsne$Y, cex=.1,main="tSNE-Capbatch",col=as.numeric(as.factor(meta[,"Capbatch"])))
 plot(tsne$Y, cex=.1,main="tSNE-Seqbatch",col=as.numeric(as.factor(meta[,"Seqbatch"])))
 plot(tsne$Y, cex=.2,main="tSNE-cluster",col=as.numeric(as.factor(meta[,"cluster"])))
