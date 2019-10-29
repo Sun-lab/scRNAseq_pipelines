df056f54bc8bfa0e7eae6b875f7f5870a1acc081
Zhang <mzhang24@fredhutch.org>
2019-10-29 10:01:47 -0700

    add kl divergence

--- DE_Autism/9.1.splatter_simulation_test.R
+++ DE_Autism/9.1.splatter_simulation_test.R
@@ -12,7 +12,7 @@ params
 #genes
 params=setParam(params, "nGenes", 5000)
 sim_splat=splatSimulate(params, batchCells=rep(200,20),verbose = FALSE)
-
+counts(sim_splat)[1:10,1:10]
 
 
 #BASiC examples
@@ -26,6 +26,7 @@ params=BASiCSEstimate(sc_example_counts[1:100, 1:30],
                          spike.info)
 params
 sim_BASiCS=BASiCSSimulate(params)
+counts(sim_BASiCS)[1:10,1:10]
 
 #lun2 examples
 library(scater)
@@ -35,7 +36,7 @@ plates=factor(sc_example_cell_info$Mutation_Status)
 params=lun2Estimate(sc_example_counts, plates, min.size = 20)
 params
 sim_lun2=lun2Simulate()
-
+counts(sim_lun2)[1:10,1:10]
 
 #lun examples
 library(scater)
@@ -43,7 +44,7 @@ data("sc_example_counts")
 params=lunEstimate(sc_example_counts)
 params
 sim_lun=lunSimulate()
-
+counts(sim_lun)[1:10,1:10]
 
 #scDD examples
 library(scater)
@@ -53,8 +54,7 @@ conditions=sample(1:2, ncol(sc_example_counts), replace = TRUE)
 params=scDDEstimate(sc_example_counts, conditions = conditions)
 params
 sim_scDD=scDDSimulate()
-
-
+counts(sim_scDD)[1:10,1:10]
 
 
 
