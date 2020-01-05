9910ca9ee57f9736e06e451d075eb52f51cf8cda
mzhang24 <mzhang24@fredhutch.org>
2020-01-05 14:45:26 -0500

    add simulation rmarkdown pipeline

---
+++ DE_Autism/9.2_DESeq2_rawcount.R
@@ -0,0 +1,95 @@
+#this code calculate the DESeq2 on the simulated count data from DCA/scvi output
+
+#cluster_tag=1
+#file_tag="3k10"
+
+
+library("DESeq2")
+#perm_num=500
+covariate_flag=NA #c(NA, "quantile99")
+perm_label=1 #perm_label =0 means calculate the observed data other wise, permutated data
+
+#setwd("~/Desktop/fh/1.Testing_scRNAseq/")
+#setwd("/Users/mzhang24/Desktop/fh/1.Testing_scRNAseq/")
+setwd("/fh/fast/sun_w/mengqi/1.Testing_scRNAseq/")
+
+###########input###############
+#input diagnosis
+if(is.na(unlist(strsplit(file_tag,"k"))[2])){
+  tmeta=meta=read.table("../Data_PRJNA434002/meta.tsv",header = TRUE, sep = "\t")
+}
+if(!is.na(unlist(strsplit(file_tag,"k"))[2])){
+  tmeta=readRDS(paste0("../Data_PRJNA434002/meta",unlist(strsplit(file_tag,"k"))[2],".rds"))
+}
+
+cur_cluster=as.character(unique(tmeta$cluster)[cluster_tag])
+meta=tmeta[tmeta$cluster==cur_cluster,]
+cur_individual=unique(meta$individual)
+
+#input counts
+rawcount_data=readRDS(paste0("../Data_PRJNA434002/rawM",file_tag,".rds"))
+rawcount_data=rawcount_data[,tmeta$cluster==cur_cluster,drop=FALSE]
+
+###################calculation t#################################
+print("start calculation: Part I: Bulk data preparation")
+
+#individual level info
+cell_num=matrix(ncol=1,nrow=length(cur_individual))
+rownames(cell_num)=cur_individual
+colnames(cell_num)="cell_num"
+read_depth=matrix(ncol=1,nrow=length(cur_individual))
+rownames(read_depth)=cur_individual
+colnames(read_depth)="read_depth"
+
+zero_rate_ind=matrix(nrow=dim(rawcount_data)[1],ncol=length(cur_individual))
+rownames(zero_rate_ind)=dimnames(rawcount_data)[[1]]
+colnames(zero_rate_ind)=cur_individual
+rawcount_matrix_bulk=matrix(nrow=dim(rawcount_data)[1],ncol=length(cur_individual))
+rownames(rawcount_matrix_bulk)=dimnames(rawcount_data)[[1]]
+colnames(rawcount_matrix_bulk)=cur_individual
+
+
+for(i_ind in 1:length(cur_individual)){
+  cur_ind=cur_individual[i_ind]
+  #fit org
+  cur_ind_m=rawcount_data[,meta$individual==cur_ind,drop=FALSE]
+  cell_num[i_ind]=dim(cur_ind_m)[2]
+  read_depth[i_ind]=sum(as.numeric(cur_ind_m),na.rm = TRUE)/cell_num[i_ind]*1000
+  
+  zero_rate_ind[,i_ind]=apply(cur_ind_m==0,1,function(x){return(sum(as.numeric(x),na.rm = TRUE))})/(cell_num[i_ind]*10)
+  rawcount_matrix_bulk[,i_ind]=apply(cur_ind_m,1,function(x){return(sum(as.numeric(x),na.rm = TRUE))})
+}
+
+saveRDS(cell_num,paste0("../Data_PRJNA434002/7.Result/rawcount_ind_cellnum_",cluster_tag,"_",file_tag,".rds"))
+saveRDS(read_depth,paste0("../Data_PRJNA434002/7.Result/rawcount_ind_readdepth_",cluster_tag,"_",file_tag,".rds"))
+saveRDS(zero_rate_ind,paste0("../Data_PRJNA434002/7.Result/rawcount_ind_zero_rate_",cluster_tag,"_",file_tag,".rds"))
+saveRDS(rawcount_matrix_bulk,paste0("../Data_PRJNA434002/7.Result/rawcount_matrix_bulk_",cluster_tag,"_",file_tag,".rds"))
+
+zero_rate=apply(rawcount_data==0,1,function(x){return(sum(as.numeric(x),na.rm = TRUE))})/(length(rawcount_data[1,]))
+read_count_total=apply(rawcount_matrix_bulk,1,function(x){return(sum(as.numeric(x),na.rm = TRUE))})
+saveRDS(zero_rate,paste0("../Data_PRJNA434002/7.Result/rawcount_gene_zero_rate_",cluster_tag,"_",file_tag,".rds"))
+saveRDS(read_count_total,paste0("../Data_PRJNA434002/7.Result/rawcount_gene_read_count_total_",cluster_tag,"_",file_tag,".rds"))
+
+cur_info=meta[,c("individual","diagnosis")]
+cur_info=unique(cur_info)
+rownames(cur_info)=cur_info$individual
+
+###################calculation t#################################
+print("start DESeq2 calculation")
+
+if(perm_label>0){
+  cur_info[,"diagnosis"]=cur_info[sample.int(nrow(cur_info),nrow(cur_info)),"diagnosis"]
+}
+
+dds=DESeqDataSetFromMatrix(countData = rawcount_matrix_bulk,
+                           colData = cur_info,
+                           design = ~ diagnosis)
+
+dds=DESeq(dds)
+de_ob_pval=results(dds)$pvalue
+
+saveRDS(de_ob_pval,paste0("../Data_PRJNA434002/8.Result/p",perm_label,"_DESeq2_ob_pval_",cluster_tag,"_",file_tag,".rds"))
+
+
+sessionInfo()
+q(save="no")
---
+++ DE_Autism/9.3_MAST_rawcount.R
@@ -0,0 +1,176 @@
+#this code calculate the MAST on the simulated count data from DCA/scvi output
+
+#cluster_tag=1
+#file_tag="3k10"
+
+
+perm_num=500
+sim_n=10
+covariate_flag=NA #c(NA, "quantile99")
+perm_label=1 #perm_label =0 means calculate the observed data other wise, permutated data
+
+#setwd("~/Desktop/fh/1.Testing_scRNAseq/")
+#setwd("/Users/mzhang24/Desktop/fh/1.Testing_scRNAseq/")
+setwd("/fh/fast/sun_w/mengqi/1.Testing_scRNAseq/")
+
+
+###########input###############
+#input diagnosis
+if(is.na(unlist(strsplit(file_tag,"k"))[2])){
+  tmeta=meta=read.table("../Data_PRJNA434002/meta.tsv",header = TRUE, sep = "\t")
+}
+if(!is.na(unlist(strsplit(file_tag,"k"))[2])){
+  tmeta=readRDS(paste0("../Data_PRJNA434002/meta",unlist(strsplit(file_tag,"k"))[2],".rds"))
+}
+
+cur_cluster=as.character(unique(tmeta$cluster)[cluster_tag])
+meta=tmeta[tmeta$cluster==cur_cluster,]
+cur_individual=unique(meta$individual)
+
+
+#input counts
+rawcount_data=readRDS(paste0("../Data_PRJNA434002/rawM",file_tag,".rds"))
+rawcount_data=rawcount_data[,tmeta$cluster==cur_cluster,drop=FALSE]
+
+dim(rawcount_data)
+dim(meta)
+###################calculation t#################################
+
+#individual level info
+CDR=matrix(ncol=1,nrow=nrow(meta))
+colnames(CDR)="CDR"
+CDR_ind=matrix(ncol=1,nrow=length(cur_individual))
+colnames(CDR_ind)="CDR_ind"
+  
+  
+for(i_ind in 1:length(cur_individual)){
+  cur_ind=cur_individual[i_ind]
+  #fit org
+  cur_ind_m=rawcount_data[,meta$individual==cur_ind]
+  cur_CDR=sum(cur_ind_m>0,na.rm = TRUE)/sum(cur_ind_m>-1,na.rm = TRUE)
+  CDR[meta$individual==cur_ind]=cur_CDR
+  CDR_ind[i_ind]=cur_CDR
+}
+
+#individual level info
+cur_individual=unique(meta$individual)
+cell_num=matrix(ncol=1,nrow=length(cur_individual))
+rownames(cell_num)=cur_individual
+colnames(cell_num)="cell_num"
+read_depth=matrix(ncol=1,nrow=length(cur_individual))
+rownames(read_depth)=cur_individual
+colnames(read_depth)="read_depth"
+
+
+zero_rate_ind=matrix(nrow=nrow(rawcount_data),ncol=length(cur_individual))
+rownames(zero_rate_ind)=rownames(rawcount_data)
+colnames(zero_rate_ind)=cur_individual
+rawcount_data_bulk=matrix(nrow=nrow(rawcount_data),ncol=length(cur_individual))
+rownames(rawcount_data_bulk)=rownames(rawcount_data)
+colnames(rawcount_data_bulk)=cur_individual
+
+for(i_ind in 1:length(cur_individual)){
+  cur_ind=cur_individual[i_ind]
+  #fit org
+  cur_ind_m=rawcount_data[,meta$individual==cur_ind]
+  cell_num[i_ind]=ncol(cur_ind_m)
+  read_depth[i_ind]=sum(cur_ind_m,na.rm = TRUE)/cell_num[i_ind]*1000
+  
+  zero_rate_ind[,i_ind]=apply(cur_ind_m==0,1,function(x){return(sum(x,na.rm = TRUE))})/cell_num[i_ind]
+  rawcount_data_bulk[,i_ind]=apply(cur_ind_m,1,function(x){return(sum(x,na.rm = TRUE))})
+}
+
+hist(read_depth)
+
+plot(read_depth,CDR_ind)
+cor(read_depth,CDR_ind)
+
+######################Other method comparison: MAST #################################
+print("start MAST calculation: Part II: ZINB KLmean and JSD")
+
+library("moments")
+rawcount_data_log = log2(1 + rawcount_data)
+
+
+dim(rawcount_data_log)
+rawcount_data_log[1:10,1:10]
+cell_id=meta$cell
+gene_id=dimnames(rawcount_data)[[1]]
+rownames(rawcount_data_log)=gene_id
+colnames(rawcount_data_log)=cell_id
+
+
+library("MAST")
+library("lme4")
+
+fData=data.frame(primerid=gene_id)
+cData=data.frame(wellKey=cell_id)
+colnames(meta)
+length(fData)
+length(cData)
+
+sca=FromMatrix(rawcount_data_log, cData, fData)
+colData(sca)$cngeneson = as.numeric(CDR) 
+colData(sca)$diagnosis =as.factor(meta$diagnosis)
+colData(sca)$ind = as.factor(meta$individual)
+
+if(perm_label>0){
+  #count cases and controls
+  diag_info=paste0(colData(sca)$ind,":",colData(sca)$diagnosis)
+  diag_kind=unique(diag_info)
+  diag_kind=t(apply(as.matrix(diag_kind),1,function(x){return(unlist(strsplit(x,":")))}))
+  
+  #permute
+  diag_kind[,2]=diag_kind[sample.int(nrow(diag_kind),nrow(diag_kind),replace=F),2]
+  
+  #match back to each individuals
+  ind_index=match(colData(sca)$ind,diag_kind[,1])
+  colData(sca)$diagnosis=as.factor(diag_kind[ind_index,2])
+}
+
+colData(sca)
+
+
+
+print(paste0("print system details, before b0"))
+date()
+gc()
+b0 = zlm(formula = ~ diagnosis, sca = sca, parallel = TRUE)
+print(paste0("print system details, before b1"))
+date()
+gc()
+b1 = zlm(formula = ~ diagnosis + ( 1 | ind ), sca = sca, method = 'glmer', 
+         ebayes = FALSE, parallel = TRUE)
+print(paste0("print system details, after b1"))
+date()
+gc()
+
+b0
+b1
+print(paste0("print system details, before lrt0"))
+date()
+gc()
+lrt0 = lrTest(b0, "diagnosis")
+print(paste0("print system details, before lrt1"))
+date()
+gc()
+lrt1 = lrTest(b1, "diagnosis")
+print(paste0("print system details, after lrt1"))
+date()
+gc()
+dim(lrt1)
+lrt1[1,,]
+
+MAST_pval0 = apply(lrt0, 1, function(x){x[3,3]})
+length(MAST_pval0)
+MAST_pval0[1:4]
+
+MAST_pval1 = apply(lrt1, 1, function(x){x[3,3]})
+length(MAST_pval1)
+MAST_pval1[1:4]
+
+saveRDS(MAST_pval0,paste0("../Data_PRJNA434002/8.Result/p",perm_label,"_MAST_pval0_rawcount_",cluster_tag,"_",file_tag,".rds"))
+saveRDS(MAST_pval1,paste0("../Data_PRJNA434002/8.Result/p",perm_label,"_MAST_pval1_rawcount_",cluster_tag,"_",file_tag,".rds"))
+
+sessionInfo()
+q(save="no")
